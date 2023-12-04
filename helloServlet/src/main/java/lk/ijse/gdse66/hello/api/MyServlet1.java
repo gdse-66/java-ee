bf0f65c0c55669bfc860ae71daa232c800185eae
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2023-12-04 22:42:43 +0530

    add http request methods

--- helloServlet/src/main/java/lk/ijse/gdse66/hello/api/MyServlet1.java
+++ helloServlet/src/main/java/lk/ijse/gdse66/hello/api/MyServlet1.java
@@ -9,8 +9,26 @@ import java.io.IOException;
 public class MyServlet1 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("MyServlet1");
-        resp.getWriter().println("I am from the MyServlet1");
-
+        System.out.println("MyServlet1 : doGet()");
+        resp.getWriter().println("I am from the MyServlet1 : doGet()");
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MyServlet1 : doPost()");
+        resp.getWriter().println("I am from the MyServlet1 : doPost()");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MyServlet1 : doPut()");
+        resp.getWriter().println("I am from the MyServlet1 : doPut()");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MyServlet1 : doDelete()");
+        resp.getWriter().println("I am from the MyServlet1 : doDelete()");
+    }
+
 }
