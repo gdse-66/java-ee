6f68786033016e18d03478be6243b88cc014d424
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2023-12-08 18:12:30 +0530

    change the load-on-startup value to test order of loading servlet

--- servlet-life-cycle/src/main/java/lk/ijse/gdse66/hello/api/Servlet2.java
+++ servlet-life-cycle/src/main/java/lk/ijse/gdse66/hello/api/Servlet2.java
@@ -2,18 +2,20 @@ package lk.ijse.gdse66.hello.api;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
 
-public class Servlet extends HttpServlet {
+
+public class Servlet2 extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        System.out.println("Servlet: init()");
+        System.out.println("Servlet2: init()");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Servlet: doGet()");
+        System.out.println("Servlet2: doGet()");
     }
 
 }
