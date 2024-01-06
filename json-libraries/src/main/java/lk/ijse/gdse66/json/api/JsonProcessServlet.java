d9f30b3bcbcace0686e96f086a16ae0c3c6d70f1
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-01-06 22:43:01 +0530

    use json library (JSON_P) - Parsson

---
+++ json-libraries/src/main/java/lk/ijse/gdse66/json/api/JsonProcessServlet.java
@@ -0,0 +1,55 @@
+package lk.ijse.gdse66.json.api;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.json.JsonReader;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebServlet(name="JsonProcessServlet", urlPatterns = "/jsonp")
+public class JsonProcessServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /* ----------------------- Read JSON--------------------*/
+        /* without json libraries */
+        /*BufferedReader reader = req.getReader();
+        String line;
+        String json = "";
+        while((line = reader.readLine()) != null ){
+            json += line + "\n";
+        }
+        System.out.println(json);*/
+
+
+        /* using JSON-P library */
+        /*JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        System.out.println(jsonObject);*/
+
+        /* ----------------------- Write JSON--------------------*/
+
+        /* using JSON-P library */
+        /*JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+
+        objectBuilder.add("id", "C001");
+        objectBuilder.add("name", "Kasun");
+        objectBuilder.add("address", "Galle");
+        JsonObject jsonObject = objectBuilder.build();*/
+
+        JsonObject jsonObject = Json.createObjectBuilder()
+                .add("id", "C001")
+                .add("name", "Kasun")
+                .add("address", "Galle").build();
+
+        resp.getWriter().write(jsonObject.toString());
+    }
+}
