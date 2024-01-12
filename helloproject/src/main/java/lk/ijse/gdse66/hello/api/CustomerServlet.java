8f4571c1c2c93276f015e2f3967346aa8cddbb55
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-01-12 14:03:10 +0530

    use json-B for hello project

--- helloproject/src/main/java/lk/ijse/gdse66/hello/api/CustomerServlet.java
+++ helloproject/src/main/java/lk/ijse/gdse66/hello/api/CustomerServlet.java
@@ -1,12 +1,15 @@
 package lk.ijse.gdse66.hello.api;
 
-import jakarta.json.*;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.gdse66.hello.dto.CustomerDTO;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
 @WebServlet(name = "Customer", urlPatterns = "/customers", loadOnStartup = 1, initParams = {
         @WebInitParam(name = "username" , value = "root"),
@@ -31,11 +34,23 @@ public class CustomerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Connection connection = null;
 
-        JsonReader reader = Json.createReader(req.getReader());
-        JsonObject jsonObject = reader.readObject();
-        String id = jsonObject.getString("id");
-        String name = jsonObject.getString("name");
-        String address = jsonObject.getString("address");
+        Jsonb jsonb = JsonbBuilder.create();
+        CustomerDTO customerDTO = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+        String id = customerDTO.getId();
+        String name = customerDTO.getName();
+        String address = customerDTO.getAddress();
+
+        /* do some validation */
+        if(id==null || !id.matches("C\\d{3}")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID is empty or invalid");
+            return;
+        } else if (name == null || !name.matches("[A-Za-z ]+")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Name is empty or invalid");
+            return;
+        } else if (address == null || address.length() < 3) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Address is empty or invalid");
+            return;
+        }
 
         System.out.printf("id=%s, name=%s, address=%s\n", id,name,address);
 
@@ -49,7 +64,13 @@ public class CustomerServlet extends HttpServlet {
             stm.setString(2, name);
             stm.setString(3, address);
 
-            stm.executeUpdate();
+            if (stm.executeUpdate() != 0) {
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+            }else {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to save the customer");
+            }
+
+
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }finally {
@@ -65,6 +86,16 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*String action = req.getParameter("action");
+        if(action ==null){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "action is empty");
+        } else-if(action.equalsIgnoreCase("GETALL")){
+            getAllCustomers();
+        } else if (action.equalsIgnoreCase("GETONE")) {
+            getCustomerById();
+        }*/
+
+
         Connection connection = null;
 
         /*create a database connection and fetch data in database*/
@@ -74,7 +105,7 @@ public class CustomerServlet extends HttpServlet {
             PreparedStatement stm = connection.prepareStatement("SELECT * FROM customer");
             ResultSet rst = stm.executeQuery();
 
-            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+            ArrayList<CustomerDTO> customerList = new ArrayList<>();
 
             while (rst.next()){
                 String id = rst.getString("id");
@@ -82,20 +113,14 @@ public class CustomerServlet extends HttpServlet {
                 String address = rst.getString("address");
                 System.out.printf("id=%s, name=%s, address=%s\n",id,name,address);
 
-                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
-                objectBuilder.add("id",id);
-                objectBuilder.add("name",name);
-                objectBuilder.add("address",address);
-                JsonObject customerJsonObject = objectBuilder.build(); //create JSON objects for each customer
-
-
-                arrayBuilder.add(customerJsonObject); // add each customer into JSON array
+                customerList.add(new CustomerDTO(id, name, address));
             }
 
-            JsonArray jsonArray = arrayBuilder.build();
-            resp.getWriter().write(jsonArray.toString()); //write JSON array in response
-
             resp.setContentType("application/json"); //set the MIME type of the content of the response (Thus, add response header called "Content-Type")
+
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(customerList,resp.getWriter());
+
 
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
@@ -121,7 +146,12 @@ public class CustomerServlet extends HttpServlet {
             connection = DriverManager.getConnection(url, username, password);
             PreparedStatement stm = connection.prepareStatement("DELETE FROM customer WHERE id=?");
             stm.setString(1,id);
-            stm.executeUpdate();
+
+            if(stm.executeUpdate() != 0){
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            }else{
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to delete the customer!");
+            }
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }finally{
@@ -140,11 +170,11 @@ public class CustomerServlet extends HttpServlet {
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Connection connection = null;
 
-        JsonReader reader = Json.createReader(req.getReader());
-        JsonObject jsonObject = reader.readObject();
-        String id = jsonObject.getString("id");
-        String name = jsonObject.getString("name");
-        String address = jsonObject.getString("address");
+        Jsonb jsonb = JsonbBuilder.create();
+        CustomerDTO customerDTO = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+        String id = customerDTO.getId();
+        String name = customerDTO.getName();
+        String address = customerDTO.getAddress();
 
         System.out.printf("id=%s, name=%s, address=%s\n", id,name,address);
 
@@ -157,7 +187,12 @@ public class CustomerServlet extends HttpServlet {
             stm.setString(2, address);
             stm.setString(3,id);
 
-            stm.executeUpdate();
+            if (stm.executeUpdate() != 0) {
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            }else{
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to update the customer");
+            }
+
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }finally {
